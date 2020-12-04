e6146fe4da3a4ce25b7bcc77c45e0d947b258e84
DocQuantum <shchurgood@gmail.com>
2020-12-04 01:39:34 -0600

    Removed unnecesary struct

--- 04/puzzle_04_01.cpp
+++ 04/puzzle_04_01.cpp
@@ -7,18 +7,6 @@
 #include <iostream>
 #include <string>
 
-struct Passport
-{
-    std::string byr = ""; //(Birth Year)
-    std::string iyr = ""; //(Issue Year)
-    std::string eyr = ""; //(Expiration Year)
-    std::string hgt = ""; //(Height)
-    std::string hcl = ""; //(Hair Color)
-    std::string ecl = ""; //(Eye Color)
-    std::string pid = ""; //(Passport ID)
-    std::string cid = ""; //(Country ID)
-};
-
 
 int main(int argc, char const *argv[])
 {
@@ -27,54 +15,45 @@ int main(int argc, char const *argv[])
     if(line.length() == 0) return 2;
 
     int valid = 0;
-    std::vector<Passport*> passports;
     auto groups = docquantum::split(line, "(\n|\n\r){2}");
     for (auto &&g : groups)
     {
         auto keyVals = docquantum::split(g, "\\s+");
-        auto pass = new Passport();
         bool found[7] = {false};
         for (auto &&kv : keyVals)
         {
             auto vals = docquantum::split(kv, ":");
             if (vals[0] == "byr")
             {
-                pass->byr = vals[1];
                 found[0] = true;
             }
             else if (vals[0] == "iyr")
             {
-                pass->iyr = vals[1];
                 found[1] = true;
             }
             else if (vals[0] == "eyr")
             {
-                pass->eyr = vals[1];
                 found[2] = true;
             }
             else if (vals[0] == "hgt")
             {
-                pass->hgt = vals[1];
                 found[3] = true;
             }
             else if (vals[0] == "hcl")
             {
-                pass->hcl = vals[1];
                 found[4] = true;
             }
             else if (vals[0] == "ecl")
             {
-                pass->ecl = vals[1];
                 found[5] = true;
             }
             else if (vals[0] == "pid")
             {
-                pass->pid = vals[1];
                 found[6] = true;
             }
             else if (vals[0] == "cid")
             {
-                pass->cid = vals[1];
+                continue;
             }
             else
             {
@@ -84,7 +63,6 @@ int main(int argc, char const *argv[])
         if (found[0] && found[1] && found[2] && found[3] && found[4] && found[5] && found[6])
             valid++;
 
-        passports.push_back(pass);
     }
 
     std::cout << "Valid passports: " << valid << std::endl;
