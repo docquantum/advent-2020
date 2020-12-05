8ff82f76a3f7dd19db72b6b658addf055cca603d
DocQuantum <shchurgood@gmail.com>
2020-12-05 17:41:56 -0600

    D5P1 implemented

---
+++ 05/puzzle_05_01.cpp
@@ -0,0 +1,58 @@
+/**
+ * DocQuantum (Daniel Shchur)
+ * Advent of Code Day 5
+ */
+
+#include "../libs/utils.hpp"
+#include <iostream>
+#include <string>
+#include <cmath>
+
+int main(int argc, char const *argv[])
+{
+    if(argc != 2) return 1;
+    auto lines = docquantum::readFileAsLines(argv[1]);
+    if(lines.size() == 0) return 2;
+
+    int highestID = -1;
+    for (auto &&l : lines)
+    {
+        float rowh = 127;
+        float rowl = 0;
+        float colh = 7;
+        float coll = 0;
+        for (size_t i = 0; i < l.length(); i++)
+        {
+            switch (l.at(i))
+            {
+            case 'F':
+                rowh /= 2;
+                rowh += rowl / 2;
+                break;
+            case 'B':
+                rowl = (rowh + rowl) / 2;
+                break;
+            case 'L':
+                colh /= 2;
+                colh += coll / 2;
+                break;
+            case 'R':
+                coll = (colh + coll) / 2;
+                break;
+            default:
+                std::cerr << "Unrecognized char! " << l.at(i) << std::endl;
+                break;
+            }
+        }
+        int row = std::round((rowh + rowl) / 2);
+        int col = std::round((colh + coll) / 2);
+        int id = row * 8 + col;
+        if (id > highestID)
+            highestID = id;
+    }
+
+    std::cout << "Highest seat ID: " << highestID << std::endl;
+
+    return 0;
+}
+
